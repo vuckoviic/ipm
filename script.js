01b63c97b873332cfc8184f9c9c4cb0f7eacc13f
vuckoviic <vuckovica64@gmail.com>
2023-10-25 18:47:12 +0200

    Remove links from header if screen width is less than 600px
    if user uses mobile phone;

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log(screen.width);
+if(screen.width < 600) {
+    const links = document.querySelector('.links');
+
+    while (links.firstChild) {
+        links.removeChild(links.firstChild);
+    }
+}
