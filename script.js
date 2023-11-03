40a2035320ab3ecd4320e795c1acf9561251b3fd
vuckoviic <vuckovica64@gmail.com>
2023-11-03 17:47:09 +0100

    Removes photo/video edits text when div is hovered

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ if(screen.width < 600) {
         links.removeChild(links.firstChild);
     }
 }
+
+const photoEdits = document.querySelector('.photo-edits');
+const photoEditsText = document.querySelector('.photo-edits-text');
+
+const videoEdits = document.querySelector('.video-edits');
+const videoEditsText = document.querySelector('.video-edits-text');
+
+
+photoEdits.addEventListener('mouseover', () => {
+    photoEdits.removeChild(photoEditsText);
+});
+
+photoEdits.addEventListener('mouseout', () => {
+    photoEdits.appendChild(photoEditsText);
+});
+
+videoEdits.addEventListener('mouseover', () => {
+    videoEdits.removeChild(videoEditsText);
+});
+
+videoEdits.addEventListener('mouseout', () => {
+    videoEdits.appendChild(videoEditsText);
+});
